4b3fdd4047ef5ee227d6bdfa354a24e06bcfb48c
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-23 09:53:04 +0530

    Add files via upload

---
+++ unit2_assignment2_question2.py
@@ -0,0 +1,67 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Given String:  BeginnersBook\n",
+      "Reversed String:  kooBsrennigeB\n"
+     ]
+    }
+   ],
+   "source": [
+    "def reverse(str):\n",
+    "    s = \"\"\n",
+    "    for ch in str:\n",
+    "        s = ch + s\n",
+    "    return s\n",
+    "\n",
+    "mystr = \"BeginnersBook\"\n",
+    "print(\"Given String: \", mystr)\n",
+    "\n",
+    "\n",
+    "print(\"Reversed String: \", reverse(mystr))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
